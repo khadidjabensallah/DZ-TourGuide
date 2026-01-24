094cefe43c208a006cf79d1f79e836b204330590
khadidjabensallah <bensalahk305@gmail.com>
2026-01-24 19:39:54 +0100

    Fix: Add sys.path to wsgi.py and remove mangled file

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Ensure the current directory is in the path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from django.core.wsgi import get_wsgi_application
 
